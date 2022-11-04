0cd4803841242c3d739b08c58b02a466c8a199d9
Simroc17 <simonerocco11@yahoo.it>
2022-11-04 18:59:01 +0100

    modifiche

--- resources/views/laravel.blade.php
+++ resources/views/laravel.blade.php
@@ -16,9 +16,10 @@
         </h1>
     </div>
     <div class="row" style="margin-left: 1300px;">
-    <li class="mb-3 ">
-    <span class="js-get-date  " style="color:#ff5050" ></span>
-    </li></div>
+        <li class="mb-3 ">
+            <span class="js-get-date  " style="color:#ff5050"></span>
+        </li>
+    </div>
 
     <div class="row">
         <div class="col-xl-12">
@@ -37,51 +38,52 @@
                     <div class="panel-content">
 
 
-                        
-                            <div class="form-group row">
-                                <label class="col-form-label col-12 col-lg-3 form-label text-lg-right">Seleziona intervallo data</label>
-                                <div class="col-12 col-lg-6">
-                                    <div class="input-group">
-                                        <input onchange="selezionaData()" type="text" class="form-control" placeholder="Select date" id="datepicker-2">
-                                        <div class="input-group-append">
-                                            <span class="input-group-text fs-xl">
-                                                <i class="fal fa-calendar"></i>
-                                            </span>
-                                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-form-label col-12 col-lg-3 form-label text-lg-right">Seleziona intervallo data</label>
+                            <div class="col-12 col-lg-6">
+                                <div class="input-group">
+                                    <input onchange="selezionaData()" type="text" class="form-control" placeholder="Select date" id="datepicker-2">
+                                    <div class="input-group-append">
+                                        <span class="input-group-text fs-xl">
+                                            <i class="fal fa-calendar"></i>
+                                        </span>
                                     </div>
                                 </div>
                             </div>
+                        </div>
 
 
-                            <div class="row">
-                                <div class="col-4">
+                        <div class="row">
+                            <div class="col-4">
 
-                                    <select id="s1" onchange="selezionaValore()" class="form-select " style="height: 35px; border-radius: 5px;">
-                                        <option selected>Scegli supermercato</option>
-                                        @foreach ($markets as $market )
+                                <select id="s1" onchange="selezionaValore()" class="form-select " style="height: 35px; border-radius: 5px;">
+                                    <option selected>Scegli supermercato</option>
+                                    @foreach ($markets as $market )
 
-                                        <option value="{{$market->id}}">
-                                            {{$market->nome}}
-                                        </option>
+                                    <option value="{{$market->id}}">
+                                        {{$market->nome}}
+                                    </option>
 
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="col-4">
-                                    <select id="s2" class="form-select" style="height: 35px; border-radius: 5px;" aria-label="Default select example">
-                                        <option selected>Open this select menu</option>
-                                        @foreach ($marketsAll as $market )
-                                        <option value="{{$market->id_parent}}" style="display:none;">
-                                            {{$market->nome}}
-                                        </option>
+                                    @endforeach
 
-                                        @endforeach
-
-                                    </select>
-                                </div>
-                                <button id ="bottone" class="btn btn-primary col-1"  style="background-color: red; border-color: red;">cerca</button>
+                                </select>
                             </div>
-                        
+                            <div class="col-4">
+                                <select id="s2" class="form-select" style="height: 35px; border-radius: 5px;" aria-label="Default select example">
+                                    <option selected>Open this select menu</option>
+                                    <option selected>Seleziona tutti</option>
+                                    @foreach ($marketsAll as $market )
+                                    <option value="{{$market->id_parent}}" style="display:none;">
+                                        {{$market->nome}}
+                                    </option>
+                                    @endforeach
+
+                                </select>
+                            </div>
+                            <button id="bottone" class="btn btn-primary col-1" style="background-color: red; border-color: red;">cerca</button>
+                        </div>
+
                     </div>
                 </div>
 
@@ -107,56 +109,46 @@
                         <input class="search " placeholder="Search" />
                         <button class="  sort btn btn-primary col-2  " data-sort="nome" style="background-color: red; border-color: red; "> Aggiorna </button>
                         <!-- datatable start -->
-                        
-                        <table id="test-list" style="display: none;" class ="table table-bordered table-hover table-striped w-100 mt-1">
+
+                        <table id="test-list" style="display: none;" class="table table-bordered table-hover table-striped w-100 mt-1">
                             <thead>
                                 <tr class="text-white" role="row" style="background-color: red;">
-                                    <th  tabindex="0"  rowspan="1" colspan="1" style="width: 5%;"  >Loghi</th>
-                                    <th  tabindex="0"  rowspan="1" colspan="1" style="width: 30%;"  >Promo</th>
-                                    <th  tabindex="0"  rowspan="1" colspan="1" style="width: 35%;" aria-label="Position: activate to sort column ascending">Descrizione</th>
-                                    <th  tabindex="0"  rowspan="1" colspan="1" style="width: 15%;" aria-label="Office: activate to sort column ascending">Data inizio</th>
-                                    <th  tabindex="0"  rowspan="1" colspan="1" style="width: 15%;" aria-label="Age: activate to sort column ascending">Data fine</th>
-                                    
+                                    <th tabindex="0" rowspan="1" colspan="1" style="width: 5%;">Loghi</th>
+                                    <th tabindex="0" rowspan="1" colspan="1" style="width: 30%;">Promo</th>
+                                    <th tabindex="0" rowspan="1" colspan="1" style="width: 35%;" aria-label="Position: activate to sort column ascending">Descrizione</th>
+                                    <th tabindex="0" rowspan="1" colspan="1" style="width: 15%;" aria-label="Office: activate to sort column ascending">Data inizio</th>
+                                    <th tabindex="0" rowspan="1" colspan="1" style="width: 15%;" aria-label="Age: activate to sort column ascending">Data fine</th>
+
                                 </tr>
                             </thead>
-                            <tbody id="tbody" class="list " >
+                            <tbody id="tbody" class="list ">
                                 @if (count($arrayPromo)>0)
                                 @foreach ($arrayPromo as $promozione )
                                 <tr>
-                                     <td>
-
-                                    @if ($promozione->id_canale==75) 
-                                            <img src="img\deco.png" alt="" style="width: 50px;">
-
-                                         @elseif ($promozione->id_canale==92) 
-                                            <img src="img\sebon.png" alt="" style="width: 50px;">
-
-                                         @elseif ($promozione->id_canale==141) 
-                                            <img src="img\ayoka.png" alt="" style="width: 50px;">
-
-                                         @else($promozione->id_canale==143)
-                                            <img src="" alt="" style="width: 50px;">
-
-
-
-
-                                    @endif
-                                     </td>
+                                    <td style="width: 5%;">
+                                        @if ($promozione->id_canale==75)
+                                        <img src="img\decoNuova.png" alt="" style="width: 50px;">
+                                        @elseif ($promozione->id_canale==92)
+                                        <img src="img\sebonNuova.png" alt="" style="width: 50px;">
+                                        @elseif ($promozione->id_canale==141)
+                                        <img src="img\ayokaNuova.png" alt="" style="width: 50px;">
+                                        @else($promozione->id_canale==143)
+                                        <img src="" alt="" style="width: 50px;">
+                                        @endif
+                                    </td>
                                     <td class="nome">{{ $promozione['nome'] }}</td>
                                     <td class="descrizione">{{ $promozione['descrizione'] }}</td>
-                                    <td >{{ $promozione['date_start'] }}</td>
+                                    <td>{{ $promozione['date_start'] }}</td>
                                     <td>{{ $promozione['date_end'] }}</td>
-
-
                                 </tr>
-
                                 @endforeach
-
                                 @endif
-
                             </tbody>
-                            <tfoot class="pagination"></tfoot>
+                            <div>
+                            <tfoot class="pagination" style="width:50px"></tfoot>
+                            </div>
                         </table>
+                        
                         <!-- datatable end -->
                     </div>
                 </div>
@@ -201,7 +193,7 @@
             }
         }
     }
-    
+
 
     function selezionaData() {
         var dataSelezionata = document.getElementById("datepicker-2").value;
@@ -209,7 +201,7 @@
         // Spezzare la data e salvarla in un array QUI
         let myArray = dataSelezionata.split(" - ");
         // console.log(myArray);
-        
+
         // console.log(dateStart);
         // console.log(dateEnd);
         return myArray;
@@ -242,22 +234,22 @@
         var tabella = document.getElementById('test-list');
 
         // Funzione Ajax al change dell'option
-        $('#s1').on('change', function() {
-            var category = $(this).val();
+        $('#bottone').on('click', function() {
+            var category = $('#s1').val();
+
+           
+
             
-            // $('#bottone').on('click', function() {
 
-                // $("#tbody").empty();  ///Parti da qui  
-
-            let arrayDate =selezionaData();
+            let arrayDate = selezionaData();
             // console.log(arrayDate);
             let dateStart = arrayDate[0];
             let dateEnd = arrayDate[1];
             // console.log(typeof(dateStart), dateStart);
             // console.log(dateEnd);
             tabella.style.display = 'block';
-            tabella.style='col-12';
-            
+            tabella.style = 'col-12';
+
             
             $.ajax({
                 url: "/{filename}",
@@ -266,25 +258,25 @@
                     'category': category,
                     'dateStart': dateStart,
                     'dateEnd': dateEnd
-                    
-                    
+
+
                 },
                 success: function(data) {
                     var promozioni = data.promozioni;
                     // console.log(promozioni)
                     var html = '';
-                     
+
                     if (promozioni.length > 0) {
                         for (let i = 0; i < promozioni.length; i++) {
-                            html += '`<tr>\
-                                 <td class="img' + promozioni[i]['id_canale'] +'">  </td>\
-                                 <td class="nome"> ' + promozioni[i]['nome'] + ' </td>\
-                                 <td class="descrizione"> ' + promozioni[i]['descrizione'] + ' </td>\
-                                 <td class="date_start"> ' + promozioni[i]['date_start'] + ' </td>\
-                                 <td> ' + promozioni[i]['date_end'] + ' </td>\
+                            html += '`<tr role="row">\
+                                 <td style="width:5%;" class="img' + promozioni[i]['id_canale'] + ' w3-round-xxlarge">  </td>\
+                                 <td style="width:30%;" class="nome"> ' + promozioni[i]['nome'] + ' </td>\
+                                 <td style="width:35%;" class="descrizione"> ' + promozioni[i]['descrizione'] + ' </td>\
+                                 <td style="width:15%;" class="date_start"> ' + promozioni[i]['date_start'] + ' </td>\
+                                 <td style="width:15%;"> ' + promozioni[i]['date_end'] + ' </td>\
                             </tr>`'
                         }
-                        
+
                     } else {
                         html += '<tr>\
                                  <td> Nessuna promozione trovata</td>\
@@ -295,11 +287,11 @@
                     };
 
                     var userList = new List('users', options);
-                    
-                    
+
+
                     $("#tbody").html(html);
                     // Ricerca dopo filtro supermercato
-                
+
                     var userList = new List('users', options);
                     //Prova pagination
                     var monkeyList = new List('test-list', {
@@ -309,37 +301,53 @@
                     });
                 }
             });
-           });
         });
-    // });
+    });
+   
 </script>
 
 <style>
+    /* td img {
+        width: 100%;;
+    } */
+    .img75 {
+        background-image: url("img/ayokaNuova.png");
+        background-size: 60px;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-position-x: center;
+        border: 2px solid;
+        width: 100%;;
 
-    .img75{
-        background-image: url("img/deco.png");
+    }
+
+    .img92 {
+        background-image: url("img/ayokaNuova.png");
         background-size: 60px;
         background-position: center;
         background-repeat: no-repeat;
         background-position-x: center;
         border: 2px solid;
-        
-    }.img92{
-        background-image: url("img/sebon.png");
+        width: 100%;;
+    }
+
+    .img141 {
+        background-image: url("img/ayokaNuova.png");
         background-size: 60px;
         background-position: center;
         background-repeat: no-repeat;
         background-position-x: center;
         border: 2px solid;
-        
-    }.img141{
-        background-image: url("img/ayoka.png");
+        width: 100%;;
+    }
+    .img143 {
+        background-image: url("img/ayokaNuova.png");
         background-size: 60px;
         background-position: center;
         background-repeat: no-repeat;
         background-position-x: center;
         border: 2px solid;
-        
+        width: 100%;;
     }
 
     .pagination li a {
